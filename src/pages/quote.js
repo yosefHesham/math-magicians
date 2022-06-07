4b8f168d8197b44ffa786b1cceb7f852c9d4aa48
yosefHesham <yousefmobdev@gmail.com>
2022-06-07 18:44:39 +0200

    Style quote

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Quote = () => {
-  return (<div class="quote">
+  return (<div className="quote">
     Mathmatics is not about numbers, equations, computations, or algorithms:
     its about understanding. - William Paul Thurston
   </div>)
